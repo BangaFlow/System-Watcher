2736e457fb25bd541eb11a9c2c88f4114d20096f
Khaled Saidi <yuko@pop-os.localdomain>
2020-08-04 08:42:40 +0100

    router setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,49 @@
 import React from 'react';
+import { 
+  NavLink, 
+  Switch,
+  Route
+} from 'react-router-dom'
 import './App.css';
 import User from './components/users/User';
 
 function App() {
   return (
     <div>
-      <User />
+      <nav>
+        <ul>
+          <li>
+            <NavLink exact to="/" activeClassName="hurray">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" activeClassName="hurray">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/users" activeClassName="hurray">Users</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <User />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+      </Switch>
     </div>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
 }
 
 export default App;
